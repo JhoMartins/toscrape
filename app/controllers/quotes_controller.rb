c59fa1556189b1064b5a866e27025af967e4980d
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-12-09 18:02:29 -0200

    implementa logica de cache e atualização de tags buscadas

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,13 @@
 class QuotesController < ApplicationController
   def index
+    tag = Tag.where(name: params[:search_tag]).first
+    if tag.present?
+      render json: tag.quotes and return if tag.searched?
+      tag.update(searched: true)
+      render json: QuotesScraping.new(params[:search_tag]).call
+    else
+      Tag.create(name: params[:search_tag], searched: true)
+      render json: QuotesScraping.new(params[:search_tag]).call
+    end
   end
 end
