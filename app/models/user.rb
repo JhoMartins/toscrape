38456009e66a3ed1b2d395467c9bbc0c401351db
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-12-09 14:05:04 -0200

    configura simple_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User
   include Mongoid::Document
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  acts_as_token_authenticatable
+  field :authentication_token
+  
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
-
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
-
   field :remember_created_at, type: Time
 end
