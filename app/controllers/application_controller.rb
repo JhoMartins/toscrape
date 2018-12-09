741ad4aa41d008b71b278f8f0284d2b500a72914
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-12-09 15:43:01 -0200

    implementa autenticação via token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  prepend_before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, _options|
+      User.where(authentication_token: token).present?
+    end
+  end
 end
