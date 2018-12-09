22920dddad410521321c10951751fd7bdef88945
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-12-09 19:30:57 -0200

    sobrescreve serializador as_json para formatar corretamente as chaves

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   def index
     tag = Tag.where(name: params[:search_tag]).first
     if tag.present?
-      render json: tag.quotes and return if tag.searched?
+      render json: tag.quotes.as_json(methods: :tags_serialized, except: [:_id, :tag_ids]) and return if tag.searched?
       tag.update(searched: true)
       render json: QuotesScraping.new(params[:search_tag]).call
     else
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -10,5 +10,16 @@ class Quote
   validates :quote, presence: :true, uniqueness: true
   validates :author, presence: true
   validates :author_about, presence: true
+
+  def as_json(options={})
+    attrs = super(options)
+    attrs["tags"] = attrs["tags_serialized"]
+    attrs.delete("tags_serialized")
+    attrs
+  end
+
+  def tags_serialized
+    tags.map(&:name)
+  end
 end
     
