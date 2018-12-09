268a513d6300d79048bfc89f8ca75d862a8ecbeb
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-12-09 16:59:42 -0200

    configuraçãoes sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :users, only: :create
 
   get "/quotes/:search_tag", to: "quotes#index"
+
+  mount Sidekiq::Web => '/sidekiq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
